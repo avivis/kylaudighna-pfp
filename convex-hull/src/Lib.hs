ca93d7e066c6f5631baad671a9b1e66b59ea679c
claudiacortell <cccortell@gmail.com>
2024-11-20 11:05:07 -0500

    Point2D and Line2D code

--- convex-hull/src/Lib.hs
+++ convex-hull/src/Lib.hs
@@ -1,6 +1,12 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+  Point2D (Point2D),
+  Line2D (Line2D),
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- TODO: Turn point and line into classes so we can easily switch between 2D and 3D
+
+data Point2D = Point2D Double Double deriving (Show)
+data Line2D = Line2D Point2D Point2D deriving (Show)
+
+-- someFunc :: IO ()
+-- someFunc = putStrLn "someFunc"
