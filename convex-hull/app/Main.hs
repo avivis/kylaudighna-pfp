d8ad73cd86916a0af11887e39f4627103795a973
KillyBOT <edwards.kyle.j@gmail.com>
2024-11-19 23:13:46 -0500

    Finished 2D quickhull sequential

--- convex-hull/app/Main.hs
+++ convex-hull/app/Main.hs
@@ -1,6 +1,34 @@
 module Main (main) where
 
+--
+-- import Lib
+--
+-- main :: IO ()
+-- main = someFunc
+
 import Lib
+import QuickHull (quickHull)
+import System.Random
+
+randomDouble :: Int -> [Double]
+randomDouble seed = randoms (mkStdGen seed) :: [Double]
+
+odds :: [a] -> [a]
+odds [] = []
+odds [x] = [x]
+odds (x : _ : xs) = x : odds xs
+
+evens :: [a] -> [a]
+evens [] = []
+evens [_] = []
+evens (_ : x : xs) = x : evens xs
+
+testConvexHullFn :: ([Point2D] -> [Point2D]) -> Int -> Int -> [Point2D]
+testConvexHullFn f seed n =
+  let randomVals = take (2 * n) $ randomDouble seed
+      points = zipWith Point2D (odds randomVals) (evens randomVals)
+   in f points
 
 main :: IO ()
-main = someFunc
+main = do
+  print $ testConvexHullFn quickHull 3 65536
--- convex-hull/test/Spec.hs
+++ convex-hull/test/Spec.hs
@@ -1,2 +1,29 @@
+import Lib
+import QuickHull (quickHull)
+import System.Random
+
+randomDouble :: Integer -> [Double]
+randomDouble seed = randoms (mkStdGen seed) :: [Double]
+
+odds :: [a] -> [a]
+odds [] = []
+odds [x] = [x]
+odds (x : _ : xs) = x : odds xs
+
+evens :: [a] -> [a]
+evens [] = []
+evens [_] = []
+evens (_ : x : xs) = x : evens xs
+
+testConvexHullFn :: ([Point2D] -> [Point2D]) -> Integer -> Integer -> [Point2D]
+testConvexHullFn f seed n =
+  let randomVals = take (2 * n) $ randomDouble seed
+      points = zip (odds randomVals) (evens randomVals)
+   in f points
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  let pointNum = 65536
+      randomVals = take (2 * pointNum) randomDouble
+      points = zip (odds randomVals) (evens randomVals)
+   in print $ quickHull points
