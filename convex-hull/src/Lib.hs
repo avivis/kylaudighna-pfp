e4dee4da7d509a628fda526ba38a5a76a18e9059
claudiacortell <cccortell@gmail.com>
2024-11-20 14:07:32 -0500

    Done with Graham Scan sequential

--- convex-hull/src/Lib.hs
+++ convex-hull/src/Lib.hs
@@ -6,7 +6,7 @@ module Lib (
 -- TODO: Turn point and line into classes so we can easily switch between 2D and 3D
 
 data Point2D = Point2D Double Double deriving (Show)
-data Line2D = Line2D Point2D Point2D deriving (Show)
+instance Eq Point2D where
+  (Point2D x1 y1) == (Point2D x2 y2) = x1 == x2 && y1 == y2
 
--- someFunc :: IO ()
--- someFunc = putStrLn "someFunc"
+data Line2D = Line2D Point2D Point2D deriving (Show)
