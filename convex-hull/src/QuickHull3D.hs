5fc2852835d29610c3ead82daa834007305d6e18
Avighna Suresh <67458389+avivis@users.noreply.github.com>
2024-12-09 12:30:31 -0800

    updated basedon paper

--- convex-hull/src/QuickHull3D.hs
+++ convex-hull/src/QuickHull3D.hs
@@ -1,44 +1,63 @@
-{-# LANGUAGE ExplicitForAll #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
-module QuickHull3D ( quickHull3, quickHull3Par) where
-
+module QuickHull3D (quickHull3, quickHull3Par) where
 import Control.DeepSeq (NFData)
 import Control.Lens ((^.))
 import Control.Parallel.Strategies (parList, rdeepseq, using)
 import Data.Function (on)
-import Data.List (maximumBy, minimumBy, nub, sortBy, delete)
+import Data.List (maximumBy, minimumBy, nub, partition)
 import Linear.V3 (V3(..), cross)
 import Linear.Metric (dot, norm, distance)
 import Linear.V3 (R1(_x))
-import Debug.Trace (trace)
+
+data Face a = Face {vertices :: (V3 a, V3 a, V3 a), outsideSet :: [(V3 a, a)], furthestPoint :: Maybe (V3 a, a)}
 
 signedVolume :: Floating a => V3 a -> V3 a -> V3 a -> V3 a -> a
-signedVolume a b c d = 
-    dot (cross (b - a) (c - a)) (d - a) / 6.0
+signedVolume a b c d = dot (cross (b - a) (c - a)) (d - a) / 6.0
 
-findPointsAbove :: (Ord a, Floating a, Show a) => a -> V3 a -> V3 a -> V3 a -> [V3 a] -> [(V3 a, a)]
-findPointsAbove epsilon p0 p1 p2 points = 
-    let volumes = [(p, vol) | p <- points, p /= p0 && p /= p1 && p /= p2, let vol = signedVolume p0 p1 p2 p, vol > epsilon]
-    in volumes
+findPointsAbove :: (Ord a, Floating a) => a -> V3 a -> V3 a -> V3 a -> [V3 a] -> [(V3 a, a)]
+findPointsAbove epsilon p0 p1 p2 points = let volumes = [(p, vol) | p <- points, p /= p0 && p /= p1 && p /= p2, let vol = signedVolume p0 p1 p2 p, vol > epsilon] in volumes
 
--- Sequential 
-quickHull3_ :: (Ord a, Floating a, Show a) => a -> [V3 a] -> V3 a -> V3 a -> V3 a -> [(V3 a, V3 a, V3 a)]
-quickHull3_ epsilon points p0 p1 p2 =
-    let pointsAbove = findPointsAbove epsilon p0 p1 p2 points
-    in if null pointsAbove
-       then [(p0, p1, p2)]
-       else let (pm, _) = maximumBy (compare `on` snd) pointsAbove
-                newPoints = map fst pointsAbove
-                processFace a b c pts = 
-                    let abovePoints = [p | p <- pts, p /= a && p /= b && p /= c, signedVolume a b c p > epsilon]
-                    in if null abovePoints
+createInitialFaces :: (Ord a, Floating a) => a -> [V3 a] -> V3 a -> V3 a -> V3 a -> V3 a -> [Face a]
+createInitialFaces epsilon points p0 p1 p2 p3 = 
+    let faces = [(p0, p1, p2), (p0, p2, p3), (p0, p3, p1), (p1, p3, p2)]
+        remainingPoints = filter (\p -> p /= p0 && p /= p1 && p/= p2 && p /= p3) points
+        createFace (v1, v2, v3) = 
+            let outside = findPointsAbove epsilon v1 v2 v3 remainingPoints
+                furthest = if null outside then Nothing else Just $ maximumBy (compare `on` snd) outside
+            in Face (v1, v2, v3) outside furthest
+    in map createFace faces
+
+processFace :: (Ord a, Floating a) => a -> Face a -> [V3 a] -> [(V3 a, V3 a, V3 a)]
+processFace epsilon face allPoints =
+    case furthestPoint face of
+        Nothing -> [vertices face]
+        Just (p, _) -> 
+            let (v1, v2, v3) = vertices face
+                newFaces = [(v1, v2, p), (v2, v3, p), (v3, v1, p)]
+                remainingPoints = map fst $ filter ((/= p) . fst) $ outsideSet face
+                processNewFace (a, b, c) = 
+                    let outside = findPointsAbove epsilon a b c remainingPoints
+                    in if null outside
                        then [(a, b, c)]
-                       else let furthest = maximumBy (compare `on` \p -> signedVolume a b c p) abovePoints
-                            in quickHull3_ epsilon abovePoints a b furthest ++ quickHull3_ epsilon abovePoints b c furthest ++ quickHull3_ epsilon abovePoints c a furthest
-            in processFace p0 p1 pm points ++ processFace p1 p2 pm points ++ processFace p2 p0 pm points
+                       else let furthest = maximumBy (compare `on` snd) outside
+                            in processNewFaceWithPoints epsilon outside furthest (a, b, c)
+            in concatMap processNewFace newFaces
 
+processNewFaceWithPoints :: (Ord a, Floating a) => a -> [(V3 a, a)] -> (V3 a, a) -> (V3 a, V3 a, V3 a) -> [(V3 a, V3 a, V3 a)]
+processNewFaceWithPoints epsilon points furthest (a, b, c) =
+    let (fp, _) = furthest
+        remaining = map fst $ filter ((/= fp) . fst) points
+    in if null remaining
+       then [(a, b, c)]
+       else let newFaces = [(a, b, fp), (b, c, fp), (c, a, fp)]
+                processSubFace (v1, v2, v3) =
+                    let above = findPointsAbove epsilon v1 v2 v3 remaining
+                    in if null above
+                       then [(v1, v2, v3)]
+                       else let (p', _) = maximumBy (compare `on` snd) above
+                            in processNewFaceWithPoints epsilon above (p', snd furthest) (v1, v2, v3)
+            in concatMap processSubFace newFaces
+
+-- Sequential
 quickHull3 :: (Ord a, Floating a, Show a) => [V3 a] -> [V3 a]
 quickHull3 points 
     | length points < 4 = points
@@ -50,52 +69,42 @@ quickHull3 points
             p2 = maximumBy (compare `on` \p -> norm (cross (p1 - p0) (p - p0))) rest1
             rest2 = filter (\p -> p /= p2) rest1
             p3 = maximumBy (compare `on` \p -> abs $ signedVolume p0 p1 p2 p) rest2
-            initialFaces = [(p0, p1, p2), (p0, p2, p3), (p0, p3, p1), (p1, p3, p2)]
-            remainingPoints = filter (\p -> p /= p0 && p /= p1 && p /= p2 && p /= p3) points
-            processFace (a, b, c) = 
-                let abovePoints = [p | p <- remainingPoints, signedVolume a b c p > epsilon]
-                in if null abovePoints
-                   then [(a, b, c)]
-                   else quickHull3_ epsilon abovePoints a b c
-            allTriangles = concatMap processFace initialFaces
-            result = nub $ concatMap (\(a,b,c) -> [a,b,c]) allTriangles
-        in result
+            initialFaces = createInitialFaces epsilon points p0 p1 p2 p3
+            allTriangles = concatMap (\f -> processFace epsilon f points) initialFaces
+        in nub $ concatMap (\(a,b,c) -> [a,b,c]) allTriangles
 
--- Parallel 
-quickHull3Par :: forall a. (Ord a, Floating a, NFData a, Show a) => [V3 a] -> [V3 a]
-quickHull3Par points = 
-    if length points < 4 
-    then points 
-    else result
-  where
-    maxDepth :: Int
-    maxDepth = 100
-    
-    epsilon :: a
-    epsilon = 1e-8
-    
-    processPoints :: (Ord a, Floating a, NFData a, Show a) => 
-                    Int -> [V3 a] -> V3 a -> V3 a -> V3 a -> [(V3 a, V3 a, V3 a)]
-    processPoints d pts p0' p1' p2' = 
-        let pointsAbove = findPointsAbove epsilon p0' p1' p2' pts
-        in if null pointsAbove || d >= maxDepth
-           then [(p0', p1', p2')]
-           else let (pm, _) = maximumBy (compare `on` snd) pointsAbove
-                    abovePoints = [p | p <- pts, p /= p0' && p /= p1' && p /= p2', signedVolume p0' p1' p2' p > epsilon]
-                    nextFaces = [(p0', p1', pm), (p1', p2', pm), (p2', p0', pm)]
-                    processFace (a, b, c) = processPoints (d + 1) abovePoints a b c
-                in if d < maxDepth `div` 2
-                   then (concat (map processFace nextFaces)) `using` parList rdeepseq
-                   else concatMap processFace nextFaces
+processPointsParallel :: (Ord a, Floating a, NFData a, Show a) => Int -> a -> [V3 a] -> Face a -> [(V3 a, V3 a, V3 a)]
+processPointsParallel depth epsilon points face =
+    case furthestPoint face of
+        Nothing -> [vertices face]
+        Just (p, _) -> 
+            let (v1, v2, v3) = vertices face
+                newFaces = [(v1, v2, p), (v2, v3, p), (v3, v1, p)]
+                remainingPoints = map fst $ filter ((/= p) . fst) $ outsideSet face
+                processNewFace (a, b, c) = 
+                    let outside = findPointsAbove epsilon a b c remainingPoints
+                        newFace = Face (a,b,c) outside (if null outside 
+                                                      then Nothing 
+                                                      else Just $ maximumBy (compare `on` snd) outside)
+                    in if depth < 50
+                       then processPointsParallel (depth + 1) epsilon points newFace
+                       else processFace epsilon newFace points
+            in if depth < 2
+               then concat (map processNewFace newFaces `using` parList rdeepseq)
+               else concatMap processNewFace newFaces
 
-    p0 = minimumBy (compare `on` (^._x)) points
-    p1 = maximumBy (compare `on` distance p0) points
-    rest1 = filter (\p -> p /= p0 && p /= p1) points
-    p2 = maximumBy (compare `on` \p -> norm (cross (p1 - p0) (p - p0))) rest1
-    rest2 = filter (\p -> p /= p2) rest1
-    p3 = maximumBy (compare `on` \p -> abs $ signedVolume p0 p1 p2 p) rest2
-    initialFaces = [(p0, p1, p2), (p0, p2, p3), (p0, p3, p1), (p1, p3, p2)]
-    remainingPoints = filter (\p -> p /= p0 && p /= p1 && p /= p2 && p /= p3) points
-    allTriangles = (concat (map (\(a,b,c) -> processPoints 1 remainingPoints a b c) initialFaces)) 
-                   `using` parList rdeepseq
-    result = nub $ concatMap (\(a,b,c) -> [a,b,c]) allTriangles
+-- Parallel
+quickHull3Par :: (Ord a, Floating a, NFData a, Show a) => [V3 a] -> [V3 a]
+quickHull3Par points 
+    | length points < 4 = points
+    | otherwise = 
+        let epsilon = 1e-8
+            p0 = minimumBy (compare `on` (^._x)) points
+            p1 = maximumBy (compare `on` distance p0) points
+            rest1 = filter (\p -> p /= p0 && p /= p1) points
+            p2 = maximumBy (compare `on` \p -> norm (cross (p1 - p0) (p - p0))) rest1
+            rest2 = filter (\p -> p /= p2) rest1
+            p3 = maximumBy (compare `on` \p -> abs $ signedVolume p0 p1 p2 p) rest2
+            initialFaces = createInitialFaces epsilon points p0 p1 p2 p3
+            allTriangles = concat (map (processPointsParallel 0 epsilon points) initialFaces `using` parList rdeepseq)
+        in nub $ concatMap (\(a,b,c) -> [a,b,c]) allTriangles
