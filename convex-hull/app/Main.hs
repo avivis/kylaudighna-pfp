d08a45c9a7316acf2809896802203ea9b7dd980d
Avighna Suresh <67458389+avivis@users.noreply.github.com>
2024-12-14 02:34:57 -0500

    k

--- convex-hull/app/Main.hs
+++ convex-hull/app/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
-import Linear.V2 (V2(..))
-import Linear.V3 (V3(..))
+import Linear.V2
+import Linear.V3
 import GrahamScan (grahamScan)
 import QuickHull (quickHull2, quickHull2Par)
 import QuickHull3D (quickHull3, quickHull3Par)
@@ -9,58 +9,44 @@ import Chans (chans2, chans2Par)
 import System.Environment (getArgs)
 import System.Random
 
-randomV2s :: (RandomGen g, Random a) => g -> [V2 a]
+randomV2s :: (RandomGen g, Random a, Fractional a) => g -> [V2 a]
 randomV2s gen =
-  let (x, gen') = random gen
-      (y, gen'') = random gen'
+  let (x, gen') = randomR (0, 1) gen
+      (y, gen'') = randomR (0, 1) gen'
    in V2 x y : randomV2s gen''
 
-randomV3s :: (RandomGen g, Random a) => g -> [V3 a]
+randomV3s :: (RandomGen g, Random a, Fractional a) => g -> [V3 a]
 randomV3s gen =
-  let (x, gen') = random gen
-      (y, gen'') = random gen'
-      (z, gen''') = random gen''
+  let (x, gen') = randomR (0, 1) gen
+      (y, gen'') = randomR (0, 1) gen'
+      (z, gen''') = randomR (0, 1) gen''
    in V3 x y z : randomV3s gen'''
-
-printPoints :: Show a => [a] -> IO ()
-printPoints points = do
-    putStrLn "generated points:"
-    mapM_ (putStrLn . show) points
-    putStrLn $ "total points: " ++ show (length points)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [numPointsString, algorithm, dimension] -> do
-      let numPoints = read numPointsString :: Int
-      putStrLn $ "generating " ++ show numPoints ++ " points..."
-      case dimension of
-        "2d" -> do
-          let points = take numPoints $ randomV2s (mkStdGen 3) :: [V2 Double]
-          putStrLn "running 2D algorithm..."
+    [numPointsString, algorithm, printFlag] -> do
+      let n = read numPointsString :: Int
+          points2d = take n $ randomV2s (mkStdGen 5) :: [V2 Double]
+          points3d = take n $ randomV3s (mkStdGen 5) :: [V3 Double]
+      case printFlag of
+        "print" -> do
+          putStrLn "Original points:"
           case algorithm of
-            "grahamScan" -> print $ grahamScan points
-            "quickHull" -> print $ quickHull2 points
-            "quickHullPar" -> print $ quickHull2Par points
-            "chans" -> print $ chans2 points
-            "chansPar" -> print $ chans2Par numPoints points
-            _ -> putStrLn "invalid algorithm"
-        "3d" -> do
-          let points = take numPoints $ randomV3s (mkStdGen 3) :: [V3 Double]
-          printPoints points
-          putStrLn "running 3D algorithm..."
-          case algorithm of
-            "quickHull" -> do
-                putStrLn "Starting QuickHull 3D..."
-                let result = quickHull3 points
-                putStrLn $ "Found " ++ show (length result) ++ " vertices in convex hull"
-                print result
-            "quickHullPar" -> do
-                putStrLn "Starting Parallel QuickHull 3D..."
-                let result = quickHull3Par points
-                putStrLn $ "Found " ++ show (length result) ++ " vertices in convex hull"
-                print result
-            _ -> putStrLn "invalid algorithm"
-        _ -> putStrLn "choose: 2d or 3d"
-    _ -> putStrLn "usage: convex-hull <numPoints> <algorithm> <dimension>"
+            "quickHull3" -> mapM_ print points3d
+            "quickHull3Par" -> mapM_ print points3d
+            _ -> mapM_ print points2d
+        "no-print" -> return ()
+        _ -> putStrLn "Invalid print flag, choose: print or no-print."
+      putStrLn "\nConvex hull:"
+      case algorithm of
+        "grahamScan" -> mapM_ print $ grahamScan points2d
+        "quickHull2" -> mapM_ print $ quickHull2 points2d
+        "quickHull2Par" -> mapM_ print $ quickHull2Par points2d
+        "chans" -> mapM_ print $ chans2 n points2d
+        "chansPar" ->  mapM_ print $ chans2Par n points2d
+        "quickHull3" -> mapM_ print $ quickHull3 points3d
+        "quickHull3Par" -> mapM_ print $ quickHull3Par points3d
+        _ -> putStrLn "Invalid algorithm, choose: grahamScan, quickHull2, quickHullPar2, chans, chansPar, quickHull3, or quickHull3Par."
+    _ -> putStrLn "usage: convex-hull <numPoints> <algorithm> <print-flag>"
