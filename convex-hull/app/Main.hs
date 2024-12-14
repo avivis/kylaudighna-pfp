7dcc4436d383f83ef1c8010fc41b48591cbc68d0
claudiacortell <cccortell@gmail.com>
2024-12-13 19:34:47 -0500

    printing points with mapM_ instead

--- convex-hull/app/Main.hs
+++ convex-hull/app/Main.hs
@@ -9,19 +9,18 @@ import Chans (chans2, chans2Par)
 import System.Environment (getArgs)
 import System.Random
 
-randomV2s :: (RandomGen g, Random a) => g -> [V2 a]
+randomV2s :: (RandomGen g, Random a, Fractional a) => g -> [V2 a]
 randomV2s gen =
-  let (x, gen') = random gen
-      (y, gen'') = random gen'
+  let (x, gen') = randomR (0.1, 1) gen
+      (y, gen'') = randomR (0.1, 1) gen'
    in V2 x y : randomV2s gen''
 
-randomV3s :: (RandomGen g, Random a) => g -> [V3 a]
+randomV3s :: (RandomGen g, Random a, Fractional a) => g -> [V3 a]
 randomV3s gen =
-  let (x, gen') = random gen
-      (y, gen'') = random gen'
-      (z, gen''') = random gen''
+  let (x, gen') = randomR (0, 1) gen
+      (y, gen'') = randomR (0, 1) gen'
+      (z, gen''') = randomR (0.1, 1) gen''
    in V3 x y z : randomV3s gen'''
-
 
 main :: IO ()
 main = do
@@ -32,12 +31,12 @@ main = do
           points2d = take n $ randomV2s (mkStdGen 3) :: [V2 Double]
           points3d = take n $ randomV3s (mkStdGen 3) :: [V3 Double]
       case algorithm of
-        "grahamScan" -> print $ grahamScan points2d
-        "quickHull2" -> print $ quickHull2 points2d
-        "quickHull2Par" -> print $ quickHull2Par points2d
-        "chans" -> print $ chans2 n points2d
-        "chansPar" ->  print $ chans2Par n points2d
-        "quickHull3" -> print $ quickHull3 points3d
-        "quickHull3Par" -> print $ quickHull3Par points3d
+        "grahamScan" -> mapM_ print $ grahamScan points2d
+        "quickHull2" -> mapM_ print $ quickHull2 points2d
+        "quickHull2Par" -> mapM_ print $ quickHull2Par points2d
+        "chans" -> mapM_ print $ chans2 n points2d
+        "chansPar" ->  mapM_ print $ chans2Par n points2d
+        "quickHull3" -> mapM_ print $ quickHull3 points3d
+        "quickHull3Par" -> mapM_ print $ quickHull3Par points3d
         _ -> putStrLn "Invalid algorithm, choose: grahamScan, quickHull2, quickHullPar2, chans, chansPar, quickHull3, or quickHull3Par."
     _ -> putStrLn "usage: convex-hull <numPoints> <algorithm>"
