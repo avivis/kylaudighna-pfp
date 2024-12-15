5731fe329fe58b68d8787b65678cccf78af2da77
claudiacortell <cccortell@gmail.com>
2024-12-14 21:05:54 -0500

    got newest chans

--- convex-hull/src/Chans.hs
+++ convex-hull/src/Chans.hs
@@ -27,12 +27,12 @@ quickHull2 :: (Ord a, Num a) => V.Vector (V2 a) -> V.Vector (V2 a)
 quickHull2 points =
   let
     _quickHull2 :: (Num a, Ord a) => V.Vector (V2 a) -> V2 a -> V2 a -> V.Vector (V2 a)
-    _quickHull2 v p0 p1
-      | null v = V.singleton p1
+    _quickHull2 ps p0 p1
+      | null ps = V.singleton p1
       | otherwise = _quickHull2 onRight pm p1 V.++ _quickHull2 onLeft p0 pm
      where
-      pm = maximumBy (compare `on` distFromLine2 p0 p1) v
-      (onLeft, onRightOrCenter) = V.partition ((> 0) . distFromLine2 p0 pm) v
+      pm = maximumBy (compare `on` distFromLine2 p0 p1) ps
+      (onLeft, onRightOrCenter) = V.partition ((> 0) . distFromLine2 p0 pm) ps
       onRight = V.filter ((> 0) . distFromLine2 pm p1) onRightOrCenter
 
     pXMax = maximum points
@@ -53,25 +53,25 @@ leftmostPoint o ps = ps V.! binarySearch 0 (V.length ps - 1) lPrevInit lNextInit
   -- We can save some computations here if we pass the orientations of the leftmost node, since those only need to be recalculated when l changes
   (lPrevInit, lNextInit) = compareAdjacent 0
 
-  binarySearch l r lPrev lNext
+  binarySearch l r lPrevOri lNextOri
     -- If we can't reach anymore points, we'll do l
     | l >= r = l
     -- If o->m->m-1 is not a CCW turn, and o->m->m+1 is a CCW turn, we are at the rightmost point!
-    | mPrev /= LT && mNext == GT = m
+    | mPrevOri /= LT && mNextOri == GT = m
     -- If o->l->m is a CCW turn and (o->l->l+1 is a CW turn or o->l->l-1 and o->l->l+1 turn the same way), or o->l->m is a CW turn and o->m->m-1 is a CW turn, then search to the right of m (from l to m)
-    | mSide == GT && (lNext == LT || lPrev == lNext) || mSide == LT && mPrev == LT = binarySearch l m lPrev lNext
+    | mLOri == GT && (lNextOri == LT || lPrevOri == lNextOri) || mLOri == LT && mPrevOri == LT = binarySearch l m lPrevOri lNextOri
     -- Otherwise, search to the left of m (from m+1 to r) (new lPrev = -mNext, new lNext needs to be set manually)
-    | otherwise = binarySearch (m + 1) r (compare EQ mNext) (orientation o (ps V.! (m + 1)) (ps V.! ((m + 2) `mod` V.length ps)))
+    | otherwise = binarySearch (m + 1) r (compare EQ mNextOri) (orientation o (ps V.! (m + 1)) (ps V.! ((m + 2) `mod` V.length ps)))
    where
     m = (l + r) `div` 2
-    mSide = orientation o (ps V.! l) (ps V.! m) -- Which side is m on, relative to l?
-    (mPrev, mNext) = compareAdjacent m -- What are the orientations of o->m->m-1 and o->m->m+1?
+    mLOri = orientation o (ps V.! l) (ps V.! m) -- Which side is m on, relative to l?
+    (mPrevOri, mNextOri) = compareAdjacent m -- What are the orientations of o->m->m-1 and o->m->m+1?
 
-chans2 :: (Ord a, Num a) => [V2 a] -> [V2 a]
-chans2 l = _chans2 start
+chans2 :: (Ord a, Num a) => Int -> [V2 a] -> [V2 a]
+chans2 n ps = _chans2 start
  where
-  m = (floor . sqrtDouble . fromIntegral) (length l) -- TODO: I think this is a good approximation, does anyone have a better one?
-  subPoints = chunksOf m l
+  m = 3 * (floor . sqrtDouble . fromIntegral) n -- 3 * sqrt(A) is a good approximation of the perimeter of a polygon with area A
+  subPoints = chunksOf m ps
   subHulls = map (quickHull2 . V.fromList) subPoints
   start = minimumBy (compare `on` (^. _x)) $ map (minimumBy (compare `on` (^. _x))) subHulls -- Point across all hulls with lowest X
   _chans2 p =
@@ -81,7 +81,7 @@ chans2 l = _chans2 start
 chans2Par :: (Ord a, Num a, NFData a) => Int -> [V2 a] -> [V2 a]
 chans2Par n ps = _chans2Par start
  where
-  m = floor . sqrtDouble . fromIntegral $ n
+  m = 3 * (floor . sqrtDouble . fromIntegral) n
   subPoints = chunksOf m ps
   subHulls = withStrategy (parBuffer 32 rdeepseq) (map (quickHull2 . V.fromList) subPoints) -- TODO: Play around with making this quickHull2Par, I found it was about the same speed
   start = minimumBy (compare `on` (^. _x)) $ map (minimumBy (compare `on` (^. _x))) subHulls -- Point across all hulls with lowest X
